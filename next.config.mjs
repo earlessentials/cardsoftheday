9d6e3d3de4f8741bce467f84c31c0cedf3e99f06
Pearling <258828391+earlessentials@users.noreply.github.com>
2026-06-18 19:22:42 +0700

    Prepare static export for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+const isGitHubPages = process.env.GITHUB_PAGES === "true"
+const repositoryName = "cardsoftheday"
+const githubPagesBasePath = `/${repositoryName}`
+
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  ...(isGitHubPages
+    ? {
+        basePath: githubPagesBasePath,
+        output: "export",
+        trailingSlash: true
+      }
+    : {})
 }
 
 export default nextConfig
